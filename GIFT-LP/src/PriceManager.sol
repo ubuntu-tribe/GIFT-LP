a8870315a1e8ff9d75fe29f0ca8e67b7bfe72d81
ITX Oliver Lienhard <53440401+itxcore@users.noreply.github.com>
2024-04-23 23:08:15 +0200

    Update PriceManager.sol

--- GIFT-LP/src/PriceManager.sol
+++ GIFT-LP/src/PriceManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.25;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
